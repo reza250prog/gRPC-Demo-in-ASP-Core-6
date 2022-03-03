3719f2acdd621c58279155c5062c1c24fddeafe1
Reza Abolhasanzadeh <rezaprog@yahoo.com>
2022-03-03 20:12:48 +0330

    Add project files.

---
+++ MyClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using MyServer;
+
+Console.WriteLine("Hello gRPC !");
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7039");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest
+{
+    Name = "Reza"
+});
+
+Console.WriteLine(reply.ToString());
+Console.WriteLine(reply.Message);
